eaccf15113e340d3b0d3eb363f3f2160e823653d
Roni Hershko <ronihershko0@gmail.com>
2026-02-02 12:40:16 +0200

    garbage files cleanup

--- get_masks_from_hf.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import snapshot_download
-repo_id = "roni-hershko/chess_data"
-repo_type="dataset"
-allow_patterns=["train/masks/*", "val/masks/*", "test/masks/*"]
-local_dir = "/home/avinoamd/roni/masks"
-snapshot_download(repo_id=repo_id, repo_type=repo_type, allow_patterns=allow_patterns, local_dir=local_dir)
